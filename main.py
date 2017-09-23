d3135b223013018d634aaa82afe66821b4c802ec
HeitorAlves <heitoralves07@hotmail.com>
2017-09-23 16:14:33 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from dbz import *
+from cosmetico import *
+
+
+def dummy_agent(info):
+    return DIREITA
+
+
+def IA_agente()
+
+
+if __name__ == "__main__":
+    dbz = DragonBallZ(dummy_agent)
+    executar_jogo(dbz)
